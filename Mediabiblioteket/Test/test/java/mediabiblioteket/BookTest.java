99e5b807da54eb43c488b9e42d2b9c1a91eedcbc
Mia <mia.karlsson@hotmail.se>
2020-02-13 20:19:09 +0100

    Mer test

---
+++ Mediabiblioteket/Test/test/java/mediabiblioteket/BookTest.java
@@ -0,0 +1,12 @@
+package mediabiblioteket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void listInfo() {
+    }
+}
--- Mediabiblioteket/Test/test/java/mediabiblioteket/LibraryControllerTest.java
+++ Mediabiblioteket/Test/test/java/mediabiblioteket/LibraryControllerTest.java
@@ -1,7 +1,9 @@
 package mediabiblioteket;
 
 import org.junit.jupiter.api.Test;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryControllerTest {
@@ -9,21 +11,21 @@ class LibraryControllerTest {
 LibraryController controller = new LibraryController();
 
     @Test
-    void checkUserInput_validValue_true() {
+    void checkUserInput_validValue_true_MK() {
 
         boolean test = controller.checkUserInput("121212-1212");
         assertEquals(true, test);
     }
 
     @Test
-    void checkUserInput_validInput_true() {
+    void checkUserInput_validInput_true_MK() {
 
         boolean test = controller.checkUserInput("Tolvan");
         assertEquals(true, test);
     }
 
     @Test
-    void checkUserInput_invalidCharacter_false() {
+    void checkUserInput_invalidCharacter_false_MK() {
 
         Boolean test = controller.checkUserInput("#%¤%%#¤¤&");
         assertEquals(false, test);
@@ -31,17 +33,17 @@ LibraryController controller = new LibraryController();
 
     //Testar inmatningen till mediaobjekt
     @Test
-    void checkInputOnlyDigits_Digits_True() {
+    void checkInputOnlyDigits_Digits_True_MK() {
         Boolean test = controller.checkInputOnlyDigits("121212");
         assertEquals(true, test);
     }
     @Test
-    void checkInputOnlyDigits_Letters_False() {
+    void checkInputOnlyDigits_Letters_False_MK() {
         Boolean test = controller.checkInputOnlyDigits("media");
         assertEquals(false, test);
     }
     @Test
-    void checkInputOnlyDigits_Characters_False() {
+    void checkInputOnlyDigits_Characters_False_MK() {
         Boolean test = controller.checkInputOnlyDigits("#¤#%#¤&");
         assertEquals(false, test);
     }
@@ -51,32 +53,103 @@ LibraryController controller = new LibraryController();
         assertNull();
     }*/
     @Test
-    void checkInputOnlyDigits_MinValue_False() {
+    void checkInputOnlyDigits_MinValue_False_MK() {
         Boolean test = controller.checkInputOnlyDigits("-2147483649");
         assertEquals(false, test);
     }
     @Test
-    void checkInputOnlyDigits_MaxValue_False() {
+    void checkInputOnlyDigits_MaxValue_False_MK() {
         Boolean test = controller.checkInputOnlyDigits("2147483648");
         assertEquals(false, test);
     }
 
 
     @Test
-    void writeToFile() {
+    void test_writeToFile_writeMockdataToFile_checkIfMockdataRegistered_GS() {
+        // Fyll på strukturen borrowed med data
+        String testdata = "121212-1212;123456"; // Personnummer;MedieID
+        controller.borrowed.add(testdata);
+
+        // Skriv dessa data till fil
+        controller.writeToFile();
+
+        // Läs tillbaka filen. Efter loopen skall den sista posten ligga i theLine. Det är den vi vill titta närmare på
+        String theLine="";
+        try {
+            Scanner theScanner = new Scanner(new File("Mediabiblioteket/files/Utlanade.txt"));
+
+            while (theScanner.hasNext()){
+                theLine = theScanner.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Jämför filens innehåll med det vi skickade till den
+        assertEquals(testdata, theLine);
+
+        // Ta bort inskriven testdata
+        controller.borrowed.removeLast();
+        controller.writeToFile();
     }
 
-    @Test
-    void borrowMedia() {
-    }
+   /* @Test
+    void test_borrowMedia_checkIfMediaIsBorrowed_trueIfBorrowed_GS() {
+        // Skapa testdata
+        Book testbok = new Book("Bok","Boktitel", "BokID", 1920, "Hermann Hesse");
+        Borrower testB= new Borrower("testnamn", "121212-1212", "0700900909");
+
+        // Registrera testboken som utlånad till testanvändaren
+        controller.setCurrentBorrower(testB);
+        controller.borrowMedia(testbok);
+
+        // Verifiera att testboken är utlånad
+        assertTrue(testbok.borrowed);
+
+        // Rensa bort inskriven testdata
+        controller.borrowed.removeLast();
+        controller.writeToFile();
+
+    }*/
+
+
+   // @Test
+  /*  void test_returnMedia_checkIfMediaReturnedAfterBorrowed_falseBorrowedIfReturned_GS() {
+        // Skapa testdata, bok och låntagare
+        Book testbok = new Book("Bok","Boktitel", "BokID", 1920, "Hermann Hesse");
+        Borrower testB= new Borrower("testnamn", "121212-1212", "0700900909");
+
+        // Registrera testboken som utlånad till testanvändaren
+        controller.setCurrentBorrower(testB);
+        controller.borrowMedia(testbok);
+        controller.returnMedia(testbok);
+
+        // Verifiera att testboken är utlånad
+        assertFalse(testbok.borrowed);
+
+        // Rensa bort inskriven testdata
+        //libraryController.borrowed.removeLast();
+        //libraryController.writeToFile();
+    }*/
 
     @Test
-    void returnMedia() {
+    void test_checkIfBorrowerExist_invalidBorrower_false_MK() {
+
+        String userID = "121212-1212";
+        Boolean borrowerResult = controller.checkIfBorrowerExist(userID);
+
+        assertFalse(borrowerResult);
+    }
+    @Test
+    void checkIfBorrowerExist_validBorrower_true_MK(){
+        String userID = "900118-5555";
+        Boolean borrowerResult = controller.checkIfBorrowerExist(userID);
+
+        assertTrue(borrowerResult);
     }
 
-    @Test
-    void checkIfBorrowerExist() {
-    }
+
+
 
     @Test
     void sortMedia() {
